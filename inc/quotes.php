9f799a0f8918cce531af8b4f1b9dbbb49464ae23
Von Ralls <vonralls@Mac.lan>
2026-07-14 17:03:21 -0500

    Refactor quote engine data structure

--- inc/quotes.php
+++ inc/quotes.php
@@ -6,69 +6,89 @@
 
 function compass_quotes() {
 
-    return [
+   return [
 
-        [
-            'quote'  => 'Stay hungry. Stay foolish.',
-            'author' => 'Steve Jobs',
-        ],
+    [
+        'id' => 'stay-hungry-stay-foolish',
+        'quote' => 'Stay hungry. Stay foolish.',
+        'author' => 'Steve Jobs',
+        'category' => 'Technology',
+        'source' => 'Stanford Commencement Address (2005)',
+    ],
 
-        [
-            'quote'  => 'The map is not the territory.',
-            'author' => 'Alfred Korzybski',
-        ],
+    [
+        'id' => 'the-map-is-not-the-territory',
+        'quote' => 'The map is not the territory.',
+        'author' => 'Alfred Korzybski',
+        'category' => 'Philosophy',
+        'source' => 'Science and Sanity',
+    ],
 
-        [
-            'quote'  => 'Programs must be written for people to read.',
-            'author' => 'Harold Abelson',
-        ],
+    [
+        'id' => 'programs-must-be-written-for-people',
+        'quote' => 'Programs must be written for people to read, and only incidentally for machines to execute.',
+        'author' => 'Harold Abelson',
+        'category' => 'Programming',
+        'source' => 'Structure and Interpretation of Computer Programs',
+    ],
 
-        [
-            'quote'  => 'Make it work. Make it right. Make it fast.',
-            'author' => 'Kent Beck',
-        ],
+    [
+        'id' => 'make-it-work',
+        'quote' => 'Make it work, make it right, make it fast.',
+        'author' => 'Kent Beck',
+        'category' => 'Programming',
+        'source' => 'Extreme Programming Explained',
+    ],
 
-        [
-            'quote'  => 'Simplicity is the ultimate sophistication.',
-            'author' => 'Leonardo da Vinci',
-        ],
+    [
+        'id' => 'simplicity-is-the-ultimate-sophistication',
+        'quote' => 'Simplicity is the ultimate sophistication.',
+        'author' => 'Often attributed to Leonardo da Vinci',
+        'category' => 'Design',
+        'source' => 'Common attribution',
+    ],
 
-        [
-            'quote'  => 'Walking on water and developing software from a specification are easy if both are frozen.',
-            'author' => 'Edward V. Berard',
-        ],
+    [
+        'id' => 'first-solve-the-problem',
+        'quote' => 'First, solve the problem. Then, write the code.',
+        'author' => 'John Johnson',
+        'category' => 'Programming',
+        'source' => 'Common quotation',
+    ],
 
-        [
-            'quote'  => 'First, solve the problem. Then, write the code.',
-            'author' => 'John Johnson',
-        ],
+    [
+        'id' => 'best-way-to-predict',
+        'quote' => 'The best way to predict the future is to invent it.',
+        'author' => 'Alan Kay',
+        'category' => 'Technology',
+        'source' => 'PARC',
+    ],
 
-        [
-            'quote'  => 'The best way to predict the future is to invent it.',
-            'author' => 'Alan Kay',
-        ],
+    [
+        'id' => 'perfection-is-achieved',
+        'quote' => 'Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.',
+        'author' => 'Antoine de Saint-Exupéry',
+        'category' => 'Writing',
+        'source' => 'Airman’s Odyssey',
+    ],
 
-        [
-            'quote'  => 'Perfection is achieved not when there is nothing more to add, but when there is nothing left to take away.',
-            'author' => 'Antoine de Saint-Exupéry',
-        ],
+    [
+        'id' => 'the-owls-are-not-what-they-seem',
+        'quote' => 'The owls are not what they seem.',
+        'author' => 'Log Lady',
+        'category' => 'Film & Television',
+        'source' => 'Twin Peaks',
+    ],
 
-        [
-            'quote'  => 'The impediment to action advances action. What stands in the way becomes the way.',
-            'author' => 'Marcus Aurelius',
-        ],
+    [
+        'id' => 'not-all-those-who-wander',
+        'quote' => 'Not all those who wander are lost.',
+        'author' => 'J.R.R. Tolkien',
+        'category' => 'Literature',
+        'source' => 'The Fellowship of the Ring',
+    ],
 
-        [
-            'quote'  => 'Not all those who wander are lost.',
-            'author' => 'J.R.R. Tolkien',
-        ],
-
-        [
-            'quote'  => 'The owls are not what they seem.',
-            'author' => 'Log Lady',
-        ],
-
-    ];
+];
 
 }
 
